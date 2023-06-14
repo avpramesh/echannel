b9bdeb0398590196ac53fdc2d210484cfcb16a85
avpramesh <avpramesh@gmail.com>
2023-06-14 18:41:23 +0530

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,9 +5,6 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
-import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
-import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
 import { EventListComponent } from './components/event-list/event-list.component';
 import { AddEventsComponent } from './components/add-events/add-events.component';
 import { EventDetailsComponent } from './components/event-details/event-details.component';
@@ -15,9 +12,6 @@ import { EventDetailsComponent } from './components/event-details/event-details.
 @NgModule({
   declarations: [
     AppComponent,
-    AddTutorialComponent,
-    TutorialDetailsComponent,
-    TutorialsListComponent,
     EventListComponent,
     AddEventsComponent,
     EventDetailsComponent
